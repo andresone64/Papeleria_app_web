b931bb73c72334a852b9976166a1f6304727f7d8
andresone64 <113370706+andresone64@users.noreply.github.com>
2022-09-13 12:57:40 -0500

    Update base_datos.java

--- base_datos.java
+++ base_datos.java
@@ -2,7 +2,8 @@ public class base_datos {
 
     public static void main (String [] args) {
 
-        System.out.println("Prueba uno de funcionamient de repositorio");
+        System.out.println("Prueba uno de funcionamiento de repositorio");
+        System.out.println("La prueba de repositorio ha sido exitosa.");
 
     }
 
