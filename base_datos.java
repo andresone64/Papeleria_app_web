c16709fc6b94673cfc66ef4fa4f008675a1413aa
andresone64 <113370706+andresone64@users.noreply.github.com>
2022-09-13 13:00:41 -0500

    Código fuente

--- base_datos.java
+++ base_datos.java
@@ -3,7 +3,7 @@ public class base_datos {
     public static void main (String [] args) {
 
         System.out.println("Prueba uno de funcionamiento de repositorio");
-        System.out.println("La prueba de repositorio ha sido exitosa.");
+        System.out.println("\t\tLa prueba de repositorio ha sido exitosa.");
 
     }
 
